995cc6b68067dffec808f2eec2ceaac3de543616
Yel0wPikmin <nathanael.mone@outlook.fr>
2026-04-22 23:06:42 +0200

    start_and_stop_button

--- QuickMath/Form1.Designer.cs
+++ QuickMath/Form1.Designer.cs
@@ -34,15 +34,21 @@
             MathToResolveText = new Label();
             timer1 = new System.Windows.Forms.Timer(components);
             MathUserIntupt = new TextBox();
+            MidelTextBetweenUserIntuptAndMath = new Label();
+            XPEqualLabel = new Label();
+            XPpointLabel = new Label();
+            start_and_stop_button = new Button();
             SuspendLayout();
             // 
             // TypeOfMath
             // 
             TypeOfMath.FormattingEnabled = true;
+            TypeOfMath.Items.AddRange(new object[] { "addition", "more coming !" });
             TypeOfMath.Location = new Point(12, 12);
             TypeOfMath.Name = "TypeOfMath";
             TypeOfMath.Size = new Size(121, 23);
             TypeOfMath.TabIndex = 0;
+            TypeOfMath.SelectedIndexChanged += TypeOfMath_SelectedIndexChanged;
             // 
             // MathToResolveText
             // 
@@ -50,24 +56,71 @@
             MathToResolveText.Font = new Font("Segoe UI", 16F);
             MathToResolveText.Location = new Point(12, 339);
             MathToResolveText.Name = "MathToResolveText";
-            MathToResolveText.Size = new Size(127, 30);
+            MathToResolveText.Size = new Size(52, 30);
             MathToResolveText.TabIndex = 1;
-            MathToResolveText.Text = "(Math here)";
+            MathToResolveText.Text = "1+1";
             MathToResolveText.Click += MathToResolveText_Click;
             // 
             // MathUserIntupt
             // 
             MathUserIntupt.Font = new Font("Segoe UI", 16F);
-            MathUserIntupt.Location = new Point(12, 402);
+            MathUserIntupt.Location = new Point(46, 398);
             MathUserIntupt.Name = "MathUserIntupt";
             MathUserIntupt.Size = new Size(300, 36);
             MathUserIntupt.TabIndex = 2;
+            // 
+            // MidelTextBetweenUserIntuptAndMath
+            // 
+            MidelTextBetweenUserIntuptAndMath.AutoSize = true;
+            MidelTextBetweenUserIntuptAndMath.Font = new Font("Segoe UI", 16F);
+            MidelTextBetweenUserIntuptAndMath.Location = new Point(12, 398);
+            MidelTextBetweenUserIntuptAndMath.Name = "MidelTextBetweenUserIntuptAndMath";
+            MidelTextBetweenUserIntuptAndMath.Size = new Size(28, 30);
+            MidelTextBetweenUserIntuptAndMath.TabIndex = 3;
+            MidelTextBetweenUserIntuptAndMath.Text = "=";
+            MidelTextBetweenUserIntuptAndMath.Click += label1_Click;
+            // 
+            // XPEqualLabel
+            // 
+            XPEqualLabel.AutoSize = true;
+            XPEqualLabel.Font = new Font("Segoe UI", 16F);
+            XPEqualLabel.Location = new Point(12, 50);
+            XPEqualLabel.Name = "XPEqualLabel";
+            XPEqualLabel.Size = new Size(59, 30);
+            XPEqualLabel.TabIndex = 4;
+            XPEqualLabel.Text = "XP =";
+            XPEqualLabel.Click += label1_Click_1;
+            // 
+            // XPpointLabel
+            // 
+            XPpointLabel.AutoSize = true;
+            XPpointLabel.Font = new Font("Segoe UI", 16F);
+            XPpointLabel.Location = new Point(66, 50);
+            XPpointLabel.Name = "XPpointLabel";
+            XPpointLabel.Size = new Size(25, 30);
+            XPpointLabel.TabIndex = 5;
+            XPpointLabel.Text = "0";
+            // 
+            // start_and_stop_button
+            // 
+            start_and_stop_button.Location = new Point(139, 12);
+            start_and_stop_button.Name = "start_and_stop_button";
+            start_and_stop_button.Size = new Size(75, 23);
+            start_and_stop_button.TabIndex = 6;
+            start_and_stop_button.Text = "start";
+            start_and_stop_button.UseVisualStyleBackColor = true;
+            start_and_stop_button.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
             ClientSize = new Size(800, 450);
+            Controls.Add(start_and_stop_button);
+            Controls.Add(XPpointLabel);
+            Controls.Add(XPEqualLabel);
+            Controls.Add(MidelTextBetweenUserIntuptAndMath);
             Controls.Add(MathUserIntupt);
             Controls.Add(MathToResolveText);
             Controls.Add(TypeOfMath);
@@ -84,5 +137,9 @@
         private Label MathToResolveText;
         private System.Windows.Forms.Timer timer1;
         private TextBox MathUserIntupt;
+        private Label MidelTextBetweenUserIntuptAndMath;
+        private Label XPEqualLabel;
+        private Label XPpointLabel;
+        private Button start_and_stop_button;
     }
 }
--- QuickMath/Form1.cs
+++ QuickMath/Form1.cs
@@ -11,5 +11,46 @@ namespace QuickMath
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TypeOfMath_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (TypeOfMath.SelectedItem.ToString() == "addition")
+            {
+
+
+            }
+            else if (TypeOfMath.SelectedItem.ToString() == "subtraction")
+            {
+
+            }
+            else if (TypeOfMath.SelectedItem.ToString() == "multiplication")
+            {
+
+            }
+            else if (TypeOfMath.SelectedItem.ToString() == "division")
+            {
+
+            }
+            else if (TypeOfMath.SelectedItem.ToString() == "more coming !")
+            {
+                MessageBox.Show("More math operations will be added in the future !");
+            }
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)//start_and_stop_button
+        {
+
+        }
     }
 }
