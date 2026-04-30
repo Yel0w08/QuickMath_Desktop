7155ca14aa8840e970ff3a0d119dbfc8a605309a
Yel0wPikmin <nathanael.mone@outlook.fr>
2026-04-30 22:47:43 +0200

    Update AppInfo.cs

--- QuickMath/AppInfo.cs
+++ QuickMath/AppInfo.cs
@@ -3,7 +3,7 @@ namespace QuickMath
 {
     public static class AppInfo
     {
-        public const string Version = "0.2.0";
+        public const string Version = "0.3.0";
         public const string Name = "QuickMath Desktop";
         public const string Author = "Yel0w";
     }
